d3e4c36d7c2ebe428217bcb3ca2dcf15f8c8a411
AlexLopEx03 <alejandrolopezexposito2003@gmail.com>
2025-06-13 00:41:20 +0100

    Epa

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const fetchData = async () => {
+  try {
+    const res = await fetch('http://localhost:8000/');
+    if (!res.ok) throw new Error('Error en la respuesta');
+    const data = await res.json();
+    alert(JSON.stringify(data));
+  } catch (error) {
+    alert('Error: ' + error.message);
+  }
+}
+</script>
+
+<template>
+  <button @click="fetchData">Haz fetch</button>
+</template>
