47a3d512249d2b9f9fcda7545cf2aa6e6ec7b7d0
AlexLopEx03 <164388886+AlexLopEx03@users.noreply.github.com>
2025-06-13 00:27:05 +0000

    Epa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 const fetchData = async () => {
   try {
-    const res = await fetch('http://localhost:8000/');
-    if (!res.ok) throw new Error('Error en la respuesta');
+    const res = await fetch('https://obscure-space-funicular-97x7pgg65g9cpxqx-8000.app.github.dev/');
+    if (!res.ok) {
+      const text = await res.text(); // por si no es JSON
+      throw new Error(`HTTP ${res.status}: ${text}`);
+    }
     const data = await res.json();
     alert(JSON.stringify(data));
   } catch (error) {
-    alert('Error: ' + error.message);
+    alert('Error: ' + error);
   }
 }
 </script>
+
 
 <template>
   <button @click="fetchData">Haz fetch</button>
